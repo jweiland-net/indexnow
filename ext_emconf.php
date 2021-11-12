296b4f62fde8e644908a6153b38dcb5033e14b6b
Stefan Froemken <froemken@gmail.com>
2021-11-13 00:05:25 +0100

    Update version to 0.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,11 +6,10 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'service',
     'author' => 'Stefan Froemken',
     'author_email' => 'sfroemken@jweiland.net',
-    'clearCacheOnLoad' => 1,
-    'version' => '0.0.2',
+    'version' => '0.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.19-10.5.99',
+            'typo3' => '10.4.19-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
