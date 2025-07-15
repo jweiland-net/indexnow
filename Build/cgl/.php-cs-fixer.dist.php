0df77f92528169f704e79344e042cfc5261f547e
Stefan Froemken <sfroemken@jweiland.net>
2025-07-15 11:40:12 +0200

    Add test environment

--- Build/cgl/.php-cs-fixer.dist.php
+++ Build/cgl/.php-cs-fixer.dist.php
@@ -6,7 +6,7 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 /*
- * This file is part of the package jweiland/sync-crop-areas.
+ * This file is part of the package jweiland/indexnow.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
@@ -90,7 +90,7 @@ return (new Config())
         'single_line_comment_style' => ['comment_types' => ['hash']],
         // @todo: Can be dropped once we enable @PER-CS2.0
         'single_line_empty_body' => true,
-        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+        'trailing_comma_in_multiline' => ['elements' => ['arguments', 'arrays', 'match', 'parameters']],
         'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
 
